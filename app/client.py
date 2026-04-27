2dea10397e29ee2801a64cfc227d24980b2c2942
maya bouhamdani <maya.bouhamdani0605@gmail.com>
2026-04-28 02:08:32 +0630

    Refactor SQL queries and improve print statements

--- app/client.py
+++ app/client.py
@@ -12,10 +12,12 @@ def connexion_client():
     cursor = conn.cursor()
 
     try:
-        cursor.execute(
-            "SELECT id_client FROM CLIENT WHERE email = %s AND email IN (SELECT email FROM UTILISATEUR WHERE mot_de_passe = %s)",
-            (email, mot_de_passe)
-        )
+        cursor.execute("""
+            SELECT c.id_client
+            FROM CLIENT c
+            JOIN UTILISATEUR u ON c.id_utilisateur = u.id_utilisateur
+            WHERE c.email = %s AND u.mot_de_passe = %s
+        """, (email, mot_de_passe))
         result = cursor.fetchone()
 
         if result:
@@ -55,14 +57,14 @@ def afficher_profil(id_client):
             print("Client introuvable")
             return
 
-        print("ID Client : " + str(client[0]))
-        print("Nom : " + client[1])
-        print("Prenom : " + client[2])
-        print("Email : " + client[3])
-        print("Telephone : " + str(client[4]))
-        print("Adresse : " + client[5])
-        print("Date inscription : " + str(client[6]))
-        print("Statut : " + client[7])
+        print("ID Client       : " + str(client[0]))
+        print("Nom             : " + client[1])
+        print("Prenom          : " + client[2])
+        print("Email           : " + client[3])
+        print("Telephone       : " + str(client[4]))
+        print("Adresse         : " + client[5])
+        print("Date inscription: " + str(client[6]))
+        print("Statut          : " + client[7])
 
     except Exception as e:
         print("Erreur : " + str(e))
@@ -93,13 +95,19 @@ def inscrire_client():
             print("Cet email est deja utilise")
             return
 
-        cursor.execute("INSERT INTO UTILISATEUR (email, mot_de_passe) VALUES (%s, %s)", (email, mot_de_passe))
+        cursor.execute(
+            "INSERT INTO UTILISATEUR (email, mot_de_passe) VALUES (%s, %s)",
+            (email, mot_de_passe)
+        )
         id_utilisateur = cursor.lastrowid
 
-        cursor.execute("INSERT INTO CLIENT (id_utilisateur, nom, prenom, email, telephone, adresse_livraison, date_inscription) VALUES (%s, %s, %s, %s, %s, %s, CURDATE())", (id_utilisateur, nom, prenom, email, telephone, adresse))
+        cursor.execute(
+            "INSERT INTO CLIENT (id_utilisateur, nom, prenom, email, telephone, adresse_livraison, date_inscription) VALUES (%s, %s, %s, %s, %s, %s, CURDATE())",
+            (id_utilisateur, nom, prenom, email, telephone, adresse)
+        )
 
         conn.commit()
-        print("Inscription reussie")
+        print("Inscription reussie ! Vous pouvez maintenant vous connecter.")
 
     except Exception as e:
         conn.rollback()
@@ -120,7 +128,10 @@ def modifier_client(id_client):
     cursor = conn.cursor()
 
     try:
-        cursor.execute("SELECT nom, prenom, email, telephone, adresse_livraison FROM CLIENT WHERE id_client = %s", (id_client,))
+        cursor.execute(
+            "SELECT nom, prenom, email, telephone, adresse_livraison FROM CLIENT WHERE id_client = %s",
+            (id_client,)
+        )
         client = cursor.fetchone()
 
         if not client:
@@ -128,21 +139,24 @@ def modifier_client(id_client):
             return
 
         print("Infos actuelles :")
-        print("Nom : " + client[0])
-        print("Prenom : " + client[1])
-        print("Email : " + client[2])
+        print("Nom       : " + client[0])
+        print("Prenom    : " + client[1])
+        print("Email     : " + client[2])
         print("Telephone : " + str(client[3]))
-        print("Adresse : " + client[4])
+        print("Adresse   : " + client[4])
 
         nom = input("Nouveau nom [" + client[0] + "] : ") or client[0]
         prenom = input("Nouveau prenom [" + client[1] + "] : ") or client[1]
         telephone = input("Nouveau telephone [" + str(client[3]) + "] : ") or client[3]
         adresse = input("Nouvelle adresse [" + client[4] + "] : ") or client[4]
 
-        cursor.execute("UPDATE CLIENT SET nom = %s, prenom = %s, telephone = %s, adresse_livraison = %s WHERE id_client = %s", (nom, prenom, telephone, adresse, id_client))
+        cursor.execute(
+            "UPDATE CLIENT SET nom = %s, prenom = %s, telephone = %s, adresse_livraison = %s WHERE id_client = %s",
+            (nom, prenom, telephone, adresse, id_client)
+        )
 
         conn.commit()
-        print("Informations mises a jour")
+        print("Informations mises a jour avec succes")
 
     except Exception as e:
         conn.rollback()
@@ -169,7 +183,10 @@ def desinscrire_client(id_client):
     cursor = conn.cursor()
 
     try:
-        cursor.execute("SELECT id_utilisateur FROM CLIENT WHERE id_client = %s", (id_client,))
+        cursor.execute(
+            "SELECT id_utilisateur FROM CLIENT WHERE id_client = %s",
+            (id_client,)
+        )
         result = cursor.fetchone()
         if not result:
             print("Client introuvable")
@@ -179,10 +196,13 @@ def desinscrire_client(id_client):
         cursor.execute("DELETE FROM CLIENT WHERE id_client = %s", (id_client,))
 
         if id_utilisateur:
-            cursor.execute("DELETE FROM UTILISATEUR WHERE id_utilisateur = %s", (id_utilisateur,))
+            cursor.execute(
+                "DELETE FROM UTILISATEUR WHERE id_utilisateur = %s",
+                (id_utilisateur,)
+            )
 
         conn.commit()
-        print("Desinscription effectuee. Vos donnees ont ete supprimees.")
+        print("Desinscription effectuee. Vos donnees personnelles ont ete supprimees.")
 
     except Exception as e:
         conn.rollback()
